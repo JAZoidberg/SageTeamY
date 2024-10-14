0610f83e21b4eb489a23f8cbebb17dbd93ff0754
pgsweet <pgsweet@udel.edu>
2024-10-13 23:41:16 -0400

    Basic Embed with question array and button to go to next question

--- src/commands/jobs/jobform.ts
+++ src/commands/jobs/jobform.ts
@@ -1,15 +1,68 @@
 import { Command } from '@root/src/lib/types/Command';
-import { ChatInputCommandInteraction, EmbedBuilder, InteractionResponse } from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, ChatInputCommandInteraction, EmbedBuilder, InteractionResponse } from 'discord.js';
+
+const questions = ['Question 1', 'Question 2'];
 
 export default class extends Command {
 
 	description = 'Form do get your preferences for jobs to be used with the Job Alert System!';
 
 	run(interaction: ChatInputCommandInteraction): Promise<InteractionResponse<boolean> | void> {
-		const responseEmbed = new EmbedBuilder()
+		let questionNum = 0;
+
+		const nextButton = new ButtonBuilder()
+			.setCustomId('nextQuestion')
+			.setLabel('Next Question')
+			.setStyle(ButtonStyle.Primary)
+			.setEmoji('▶');
+
+		const actionRow = new ActionRowBuilder()
+			.addComponents(nextButton);
+
+		interaction.reply({
+			embeds: [this.questionGetter(questionNum)],
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			components: [actionRow]
+		});
+
+		let replyId;
+		interaction.fetchReply().then(reply => { replyId = reply.id; });
+
+		const collector = interaction.channel.createMessageComponentCollector({
+			filter: i => i.message.id === replyId
+		});
+
+		collector.on('collect', async (i: ButtonInteraction) => {
+			if (interaction.user.id !== i.user.id) {
+				await i.reply({
+					content: 'You cannot respond to a command you did not execute',
+					ephemeral: true
+				});
+				return;
+			}
+			i.deferUpdate();
+			if (i.customId === 'nextQuestion') {
+				questionNum++;
+			}
+			interaction.editReply({
+				embeds: [this.questionGetter(questionNum)],
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				components: [actionRow]
+			});
+		});
+
+		return;
+	}
+
+	questionGetter(questionNum: number): EmbedBuilder {
+		return new EmbedBuilder()
 			.setTitle('Job Alert Form')
-			.setColor('#000000');
-		return interaction.reply({ embeds: [responseEmbed] });
+			.setColor('#000000')
+			.addFields(
+				{ name: 'Question:', value: questions[questionNum] },
+				{ name: 'Answer', value: 'also test', inline: true });
 	}
 
 }
