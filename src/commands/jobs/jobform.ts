6feebb932de5bbcbdcfa5c2e54a3b22eef06f2eb
pgsweet <113042887+pgsweet@users.noreply.github.com>
2024-10-13 21:21:47 -0400

    basic Embed response message

--- src/commands/jobs/jobform.ts
+++ src/commands/jobs/jobform.ts
@@ -6,7 +6,10 @@ export default class extends Command {
 	description = 'Form do get your preferences for jobs to be used with the Job Alert System!';
 
 	run(interaction: ChatInputCommandInteraction): Promise<InteractionResponse<boolean> | void> {
-		const responseEmbed = new EmbedBuilder().setTitle('test');
+		const responseEmbed = new EmbedBuilder()
+			.setTitle('Job Alert Form')
+			.setColor('#000000');
+		
 		return interaction.reply({ embeds: [responseEmbed] });
 	}
 
