57c97f87699da25053c9b6a6e8200e347d1326cc
pgsweet <pgsweet@udel.edu>
2024-10-14 00:30:19 -0400

    Switched to using Modals for their ease of input

--- src/commands/jobs/jobform.ts
+++ src/commands/jobs/jobform.ts
@@ -1,5 +1,5 @@
 import { Command } from '@root/src/lib/types/Command';
-import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, ChatInputCommandInteraction, EmbedBuilder, InteractionResponse } from 'discord.js';
+import { ActionRowBuilder, ChatInputCommandInteraction, ComponentBuilder, InteractionResponse, ModalBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
 
 const questions = ['Question 1', 'Question 2'];
 
@@ -7,65 +7,85 @@ export default class extends Command {
 
 	description = 'Form to get your preferences for jobs to be used with the Job Alert System!';
 
-	run(interaction: ChatInputCommandInteraction): Promise<InteractionResponse<boolean> | void> {
-		let questionNum = 0;
+	async run(interaction: ChatInputCommandInteraction): Promise<InteractionResponse<boolean> | void> {
+		const questionNum = 0;
 
-		const nextButton = new ButtonBuilder()
-			.setCustomId('nextQuestion')
-			.setLabel('Next Question')
-			.setStyle(ButtonStyle.Primary)
-			.setEmoji('▶');
+		const modal = new ModalBuilder()
+			.setTitle('Job Form')
+			.setCustomId('modal');
 
-		let actionRow = new ActionRowBuilder()
-			.addComponents(nextButton);
+		const AnswerInput = new TextInputBuilder()
+			.setCustomId('answerInput')
+			.setLabel(`${questions[questionNum]}`)
+			.setStyle(TextInputStyle.Short).setPlaceholder('Answer: ');
 
-		interaction.reply({
-			embeds: [this.questionGetter(questionNum)],
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			components: [actionRow]
-		});
+		const secondActionRow = new ActionRowBuilder().addComponents(AnswerInput);
 
-		let replyId;
-		interaction.fetchReply().then(reply => { replyId = reply.id; });
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		modal.addComponents(secondActionRow);
 
-		const collector = interaction.channel.createMessageComponentCollector({
-			filter: i => i.message.id === replyId
-		});
-
-		collector.on('collect', async (i: ButtonInteraction) => {
-			if (interaction.user.id !== i.user.id) {
-				await i.reply({
-					content: 'You cannot respond to a command you did not execute',
-					ephemeral: true
-				});
-				return;
-			}
-			i.deferUpdate();
-			if (i.customId === 'nextQuestion') {
-				questionNum++;
-				actionRow = questionNum === (questions.length - 1)
-					? new ActionRowBuilder({ components: [nextButton.setDisabled(true)] })
-					: new ActionRowBuilder({ components: [nextButton] });
-			}
-			interaction.editReply({
-				embeds: [this.questionGetter(questionNum)],
-				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-				// @ts-ignore
-				components: [actionRow]
-			});
-		});
+		await interaction.showModal(modal);
 
 		return;
+		// let questionNum = 0;
+
+		// const nextButton = new ButtonBuilder()
+		// 	.setCustomId('nextQuestion')
+		// 	.setLabel('Next Question')
+		// 	.setStyle(ButtonStyle.Primary)
+		// 	.setEmoji('▶');
+
+		// let actionRow = new ActionRowBuilder()
+		// 	.addComponents(nextButton);
+
+		// interaction.reply({
+		// 	embeds: [this.questionGetter(questionNum)],
+		// 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// 	// @ts-ignore
+		// 	components: [actionRow]
+		// });
+
+		// let replyId;
+		// interaction.fetchReply().then(reply => { replyId = reply.id; });
+
+		// const collector = interaction.channel.createMessageComponentCollector({
+		// 	filter: i => i.message.id === replyId
+		// });
+
+		// collector.on('collect', async (i: ButtonInteraction) => {
+		// 	if (interaction.user.id !== i.user.id) {
+		// 		await i.reply({
+		// 			content: 'You cannot respond to a command you did not execute',
+		// 			ephemeral: true
+		// 		});
+		// 		return;
+		// 	}
+		// 	i.deferUpdate();
+		// 	if (i.customId === 'nextQuestion') {
+		// 		questionNum++;
+		// 		actionRow = questionNum === (questions.length - 1)
+		// 			? new ActionRowBuilder({ components: [nextButton.setDisabled(true)] })
+		// 			: new ActionRowBuilder({ components: [nextButton] });
+		// 	}
+		// 	interaction.editReply({
+		// 		embeds: [this.questionGetter(questionNum)],
+		// 		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// 		// @ts-ignore
+		// 		components: [actionRow]
+		// 	});
+		// });
+
+		// return;
 	}
 
-	questionGetter(questionNum: number): EmbedBuilder {
-		return new EmbedBuilder()
-			.setTitle('Job Alert Form')
-			.setColor('#000000')
-			.addFields(
-				{ name: 'Question:', value: questions[questionNum] },
-				{ name: 'Answer', value: 'also test', inline: true });
-	}
+	// questionGetter(questionNum: number): EmbedBuilder {
+	// 	return new EmbedBuilder()
+	// 		.setTitle('Job Alert Form')
+	// 		.setColor('#000000')
+	// 		.addFields(
+	// 			{ name: 'Question:', value: questions[questionNum] },
+	// 			{ name: 'Answer', value: 'also test', inline: true });
+	// }
 
 }
