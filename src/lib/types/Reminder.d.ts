dd2c7ec7762b37780ae30aadea657f7b8381a614
Hairum Qureshi <hqureshi@udel.edu>
2024-10-21 17:21:12 -0400

    reverted changes on Reminder interface

--- src/lib/types/Reminder.d.ts
+++ src/lib/types/Reminder.d.ts
@@ -2,6 +2,6 @@ export interface Reminder {
 	owner: string;
 	expires: Date;
 	content: string;
-	repeat: null | 'daily' | 'weekly';
-	mode: 'public' | 'private';
+	repeat: null | "daily" | "weekly";
+	mode: "public" | "private";
 }
