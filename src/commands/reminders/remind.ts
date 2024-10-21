21125f97bf5761e5934f9b63813bbf8b19afbaec
Hairum Qureshi <hqureshi@udel.edu>
2024-10-21 10:50:55 -0400

    modified remind command so it now utilizes subcommands with one specifically for job reminders

--- src/commands/reminders/remind.ts
+++ src/commands/reminders/remind.ts
@@ -15,49 +15,107 @@ export default class extends Command {
 	extendedHelp = "Reminders can be set to repeat daily or weekly.";
 	options: ApplicationCommandOptionData[] = [
 		{
-			name: "content",
-			description: "What you'd like to be reminded of",
-			type: ApplicationCommandOptionType.String,
-			required: true,
-		},
-		{
-			name: "reminder-type",
-			description:
-				"What kind of reminder would you like to create? Enter 'job' or 'misc'",
-			type: ApplicationCommandOptionType.String,
-			required: true,
-		},
-		{
-			name: "duration",
-			description: "When you'd like to be reminded",
-			type: ApplicationCommandOptionType.String,
-			required: true,
-		},
-		{
-			name: "repeat",
-			description: "How often you want the reminder to repeat",
-			choices: [
-				{ name: "Daily", value: "daily" },
-				{ name: "Weekly", value: "weekly" },
+			name: "create",
+			description: "Create a reminder",
+			type: ApplicationCommandOptionType.Subcommand,
+			options: [
+				{
+					name: "content",
+					description: "What you'd like to be reminded of",
+					type: ApplicationCommandOptionType.String,
+					required: true,
+				},
+				{
+					name: "duration",
+					description: "When you'd like to be reminded",
+					type: ApplicationCommandOptionType.String,
+					required: true,
+				},
+				{
+					name: "repeat",
+					description: "How often you want the reminder to repeat",
+					choices: [
+						{ name: "Daily", value: "daily" },
+						{ name: "Weekly", value: "weekly" },
+					],
+					type: ApplicationCommandOptionType.String,
+					required: false,
+				},
 			],
-			type: ApplicationCommandOptionType.String,
-			required: false,
+		},
+		{
+			name: "jobs",
+			description: "Create a reminder for a job",
+			type: ApplicationCommandOptionType.Subcommand,
+			options: [
+				{
+					name: "duration",
+					description: "When you'd like to be reminded",
+					type: ApplicationCommandOptionType.String,
+					required: true,
+				},
+				{
+					name: "repeat",
+					description: "How often you want the reminder to repeat",
+					choices: [
+						{ name: "Daily", value: "daily" },
+						{ name: "Weekly", value: "weekly" },
+					],
+					type: ApplicationCommandOptionType.String,
+					required: true,
+				},
+			],
 		},
 	];
+	// options: ApplicationCommandOptionData[] = [
+	// 	{
+	// 		name: "content",
+	// 		description: "What you'd like to be reminded of",
+	// 		type: ApplicationCommandOptionType.String,
+	// 		required: true,
+	// 	},
+	// 	{
+	// 		name: "reminder-type",
+	// 		description:
+	// "What kind of reminder would you like to create? Enter 'job' or 'misc'",
+	// 		type: ApplicationCommandOptionType.String,
+	// 		required: true,
+	// 	},
+	// {
+	// 	name: "duration",
+	// 	description: "When you'd like to be reminded",
+	// 	type: ApplicationCommandOptionType.String,
+	// 	required: true,
+	// },
+	// {
+	// 	name: "repeat",
+	// 	description: "How often you want the reminder to repeat",
+	// 	choices: [
+	// 		{ name: "Daily", value: "daily" },
+	// 		{ name: "Weekly", value: "weekly" },
+	// 	],
+	// 	type: ApplicationCommandOptionType.String,
+	// 	required: false,
+	// },
+	// ];
 
 	run(
 		interaction: ChatInputCommandInteraction
 	): Promise<InteractionResponse<boolean> | void> {
 		const content = interaction.options.getString("content");
 		const rawDuration = interaction.options.getString("duration");
-		const reminderType =
-			(interaction.options.getString("reminder-type") as
-				| "job"
-				| "misc") || null;
+		const reminderType = interaction.options.getString("reminder-type");
 		const duration = parse(rawDuration);
 		const repeat =
 			(interaction.options.getString("repeat") as "daily" | "weekly") ||
 			null;
+
+		if (!reminderType || reminderType.toLowerCase() !== "job") {
+			return interaction.reply({
+				content: `**${reminderType}** is not a valid reminder type. You can use "job" to set a job reminder or "misc" to set a reminder for something else.`,
+				ephemeral: true,
+			});
+		}
 
 		if (!duration) {
 			return interaction.reply({
@@ -66,21 +124,11 @@ export default class extends Command {
 			});
 		}
 
-		if (
-			!reminderType ||
-			reminderType.toLowerCase() != "job" ||
-			reminderType.toLocaleLowerCase() != "misc"
-		) {
-			return interaction.reply({
-				content: `**${reminderType}** is not a valid reminder type. You can use "job" to set a job reminder or "misc" to set a reminder for something else.`,
-				ephemeral: true,
-			});
-		}
-
 		const reminder: Reminder = {
 			owner: interaction.user.id,
 			content,
 			mode: "public", // temporary
+			reminderType: reminderType,
 			expires: new Date(duration + Date.now()),
 			repeat,
 		};
