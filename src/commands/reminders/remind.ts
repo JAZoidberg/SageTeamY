5d4740799b3e86a325195ba1dd712f1daebf32a5
Hairum Qureshi <hqureshi@udel.edu>
2024-10-21 00:07:35 -0400

    added another option to the reminder command that asks for the 'reminder type' and added a conditonal to check whether the user inputted a reminder type for 'job' or 'misc', otherwise they're sent an error

--- src/commands/reminders/remind.ts
+++ src/commands/reminders/remind.ts
@@ -1,60 +1,95 @@
-import { BOT, DB } from '@root/config';
-import { ApplicationCommandOptionData, ApplicationCommandOptionType, ChatInputCommandInteraction, InteractionResponse } from 'discord.js';
-import { Reminder } from '@lib/types/Reminder';
-import parse from 'parse-duration';
-import { reminderTime } from '@root/src/lib/utils/generalUtils';
-import { Command } from '@lib/types/Command';
+import { BOT, DB } from "@root/config";
+import {
+	ApplicationCommandOptionData,
+	ApplicationCommandOptionType,
+	ChatInputCommandInteraction,
+	InteractionResponse,
+} from "discord.js";
+import { Reminder } from "@lib/types/Reminder";
+import parse from "parse-duration";
+import { reminderTime } from "@root/src/lib/utils/generalUtils";
+import { Command } from "@lib/types/Command";
 
 export default class extends Command {
-
-
 	description = `Have ${BOT.NAME} give you a reminder.`;
-	extendedHelp = 'Reminders can be set to repeat daily or weekly.';
+	extendedHelp = "Reminders can be set to repeat daily or weekly.";
 	options: ApplicationCommandOptionData[] = [
 		{
-			name: 'content',
-			description: 'What you\'d like to be reminded of',
+			name: "content",
+			description: "What you'd like to be reminded of",
 			type: ApplicationCommandOptionType.String,
-			required: true
+			required: true,
 		},
 		{
-			name: 'duration',
-			description: 'When you\'d like to be reminded',
+			name: "reminder-type",
+			description:
+				"What kind of reminder would you like to create? Enter 'job' or 'misc'",
 			type: ApplicationCommandOptionType.String,
-			required: true
+			required: true,
 		},
 		{
-			name: 'repeat',
-			description: 'How often you want the reminder to repeat',
-			choices: [{ name: 'Daily', value: 'daily' }, { name: 'Weekly', value: 'weekly' }],
+			name: "duration",
+			description: "When you'd like to be reminded",
 			type: ApplicationCommandOptionType.String,
-			required: false
-		}
-	]
+			required: true,
+		},
+		{
+			name: "repeat",
+			description: "How often you want the reminder to repeat",
+			choices: [
+				{ name: "Daily", value: "daily" },
+				{ name: "Weekly", value: "weekly" },
+			],
+			type: ApplicationCommandOptionType.String,
+			required: false,
+		},
+	];
 
-	run(interaction: ChatInputCommandInteraction): Promise<InteractionResponse<boolean> | void> {
-		const content = interaction.options.getString('content');
-		const rawDuration = interaction.options.getString('duration');
+	run(
+		interaction: ChatInputCommandInteraction
+	): Promise<InteractionResponse<boolean> | void> {
+		const content = interaction.options.getString("content");
+		const rawDuration = interaction.options.getString("duration");
+		const reminderType =
+			(interaction.options.getString("reminder-type") as
+				| "job"
+				| "misc") || null;
 		const duration = parse(rawDuration);
-		const repeat = interaction.options.getString('repeat') as 'daily' | 'weekly' || null;
+		const repeat =
+			(interaction.options.getString("repeat") as "daily" | "weekly") ||
+			null;
 
 		if (!duration) {
 			return interaction.reply({
 				content: `**${rawDuration}** is not a valid duration. You can use words like hours, minutes, seconds, days, weeks, months, or years.`,
-				ephemeral: true
+				ephemeral: true,
 			});
 		}
+
+		if (
+			!reminderType ||
+			reminderType.toLowerCase() != "job" ||
+			reminderType.toLocaleLowerCase() != "misc"
+		) {
+			return interaction.reply({
+				content: `**${reminderType}** is not a valid reminder type. You can use "job" to set a job reminder or "misc" to set a reminder for something else.`,
+				ephemeral: true,
+			});
+		}
+
 		const reminder: Reminder = {
 			owner: interaction.user.id,
 			content,
-			mode: 'public', // temporary
+			mode: "public", // temporary
 			expires: new Date(duration + Date.now()),
-			repeat
+			repeat,
 		};
 
 		interaction.client.mongo.collection(DB.REMINDERS).insertOne(reminder);
 
-		return interaction.reply({ content: `I'll remind you about that at ${reminderTime(reminder)}.`, ephemeral: true });
+		return interaction.reply({
+			content: `I'll remind you about that at ${reminderTime(reminder)}.`,
+			ephemeral: true,
+		});
 	}
-
 }
