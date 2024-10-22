11ba3b9084016e6affc54618dffa73a50b800495
Hairum Qureshi <hqureshi@udel.edu>
2024-10-22 11:12:40 -0400

    added comments to jobSearchAlgorithm function

--- src/pieces/job-algorithm.ts
+++ src/pieces/job-algorithm.ts
@@ -1,3 +1,6 @@
 export default function jobSearchAlgorithm(jobAnswers) {
+	// May need to handle case inside of tasks.ts where the bot informs the user that at the moment the user has no personalized job specifications because they haven't filled out the job form (make sure to provide the user the command to utilize it)
+
+	// If they have filled out the job form, they'll receive catered job searches based on the input they provided to the form; may need to store that info in the database so that it doesn't get forgotten and stays unique for each user
 	console.log("IN ALGO:", jobAnswers);
 }
