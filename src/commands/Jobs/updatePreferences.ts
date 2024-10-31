ec6bae6c1d6890b2934a827c9e46c7d1a976592a
rahul5400 <rahulp5400@yahoo.com>
2024-10-31 11:47:56 -0400

    Copied over jobForm to updatePrefences.ts and changed some of the code to create the command to update job preferences

---
+++ src/commands/Jobs/updatePreferences.ts
@@ -0,0 +1,67 @@
+import { Command } from '@root/src/lib/types/Command';
+import { ActionRowBuilder, ApplicationCommandOptionData, ApplicationCommandOptionType,
+	ChatInputCommandInteraction, InteractionResponse, ModalBuilder, ModalSubmitFields,
+	TextInputBuilder, TextInputStyle } from 'discord.js';
+
+const questions = [
+	['What city are you located in?', 'Are you looking for remote or in person?', 'Job, internship or both?', 'How far are you willing to travel?'],
+	['Interest 1', 'Interest 2', 'Interest 3', 'Interest 4', 'Interest 5']
+];
+
+export default class extends Command {
+
+	name: 'updateJobPreferences'
+	description = 'Update your preferences for jobs to be used with the Job Alert System!';
+
+	options: ApplicationCommandOptionData[] = [
+		{
+			name: 'qset',
+			description: 'Which question set do you want to view (1 or 2).',
+			type: ApplicationCommandOptionType.Number,
+			required: true
+		}
+	]
+
+	async run(interaction: ChatInputCommandInteraction): Promise<InteractionResponse<boolean> | void> {
+		const questionSet = interaction.options.getNumber('qset') - 1;
+
+		if (questionSet !== 0 && questionSet !== 1) {
+			interaction.reply({ content: 'Please enter either 1 or 2' });
+			return;
+		}
+
+		const modal = new ModalBuilder()
+			.setCustomId(`updateJobModal${questionSet}`)
+			.setTitle(`Update Job Preferences (${questionSet + 1} of 2)`);
+
+		const askedQuestions = questions[questionSet];
+		const rows = askedQuestions.map((question) => this.getAnswerField(question, askedQuestions.indexOf(question)));
+
+		for (const row of rows) {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			modal.addComponents(row);
+		}
+
+		await interaction.showModal(modal);
+
+		// Answers are handled in src/pieces/commandManager.ts on line 149
+
+		return;
+	}
+
+
+	getAnswer(fields: ModalSubmitFields, questionNum: number): string {
+		return fields.getField(`question${questionNum + 1}`).value;
+	}
+
+
+	getAnswerField(question: string, questionNum: number): ActionRowBuilder {
+		return new ActionRowBuilder({ components: [new TextInputBuilder()
+			.setCustomId(`question${questionNum + 1}`)
+			.setLabel(`${question}`)
+			.setStyle(TextInputStyle.Short)
+			.setPlaceholder('Input Updated Answer Here')] });
+	}
+
+}
