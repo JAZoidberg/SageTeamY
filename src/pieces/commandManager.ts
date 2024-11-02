082308a5add328553d2b40de721a111bbfa30237
Jessica Cunningham <jacunnin@udel.edu>
2024-11-02 17:22:13 -0400

    changed to replace since update is moreso fields, not actually replacing yet

--- src/pieces/commandManager.ts
+++ src/pieces/commandManager.ts
@@ -169,10 +169,10 @@ async function handleModalBuilder(interaction: ModalSubmitInteraction, bot: Clie
 
 			// interaction.client.mongo.collection(DB.JOB_FORMS).insertOne(answerResponse);
 			if (answerResponse.questionSet === '0') {
-				interaction.client.mongo.collection(DB.JOB_FORMS).updateOne(
+				interaction.client.mongo.collection(DB.JOB_FORMS).replaceOne(
 					{ questionSet: 0 }, answerResponse, { upsert: true });
 			} else if (answerResponse.questionSet === '1') {
-				interaction.client.mongo.collection(DB.JOB_FORMS).updateOne(
+				interaction.client.mongo.collection(DB.JOB_FORMS).replaceOne(
 					{ questionSet: 1 }, answerResponse, { upsert: true });
 			}
 			break;
