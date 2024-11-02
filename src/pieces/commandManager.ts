b20f6ba89469edc9988738366bb542ca7cd00192
rahul5400 <rahulp5400@yahoo.com>
2024-11-02 16:40:42 -0400

    Changed the ontent of the interaction.repy to match mess

--- src/pieces/commandManager.ts
+++ src/pieces/commandManager.ts
@@ -172,8 +172,8 @@ async function handleModalBuilder(interaction: ModalSubmitInteraction, bot: Clie
 				// 	ephemeral: true
 				// });
 				await interaction.reply({
-					
-				})
+					content: success ? mess : 'Error saving preferences. Please try again'
+				});
 			} catch (error) {
 				console.error('update form error:', error);
 				await interaction.reply({ content: 'An error occurred. Please try again.', ephemeral: true });
