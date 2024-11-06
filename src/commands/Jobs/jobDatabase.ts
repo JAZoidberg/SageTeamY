5c39d36827419e137103373dc6cc83ac50540a5a
rahul5400 <rahulp5400@yahoo.com>
2024-11-05 21:55:25 -0500

    Made it so that the data would update only if new data is inserted

--- src/commands/Jobs/jobDatabase.ts
+++ src/commands/Jobs/jobDatabase.ts
@@ -1,6 +1,7 @@
 import { Collection, Db, MongoClient } from 'mongodb';
 import { DB } from '@root/config';
 import { Command } from '@root/src/lib/types/Command';
+import { Emoji } from 'discord.js';
 
 interface JobPreferences {
 	userID: string;
@@ -33,41 +34,54 @@ export class JobPreferenceAPI {
 	async storeFormResponses(userID: string, answers: string[], questionSet: number): Promise<boolean> {
 		try {
 			let updateObject = {};
-			const filterAnswers = answers.map(answer => answer.trim()).map((answer, index) => ({
-				index,
-				answer
-			})).filter(item => item.answer !== '');
+			// const filterAnswers = answers.map(answer => answer.trim()).map((answer, index) => ({
+			// 	index,
+			// 	answer
+			// })).filter(item => item.answer !== '');
 
 			// Adds answers to questions.
 			if (questionSet === 0) {
-				updateObject = {
-					'jobPreferences.answers.city': answers[0],
-					'jobPreferences.answers.workType': answers[1],
-					'jobPreferences.answers.employmentType': answers[2],
-					'jobPreferences.answers.travelDistance': answers[3]
-				};
+				// updateObject = {
+				// 	'jobPreferences.answers.city': answers[0],
+				// 	'jobPreferences.answers.workType': answers[1],
+				// 	'jobPreferences.answers.employmentType': answers[2],
+				// 	'jobPreferences.answers.travelDistance': answers[3]
+				// };
+				const [city, workType, employmentType, travelDistance] = answers;
+				if (city?.trim()) updateObject['jobPreferences.answers.city'] = city;
+				if (workType?.trim()) updateObject['jobPreferences.answers.workType'] = workType;
+				if (employmentType?.trim()) updateObject['jobPreferences.answers.employmentType'] = employmentType;
+				if (travelDistance?.trim()) updateObject['jobPreferences.answers.travelDistance'] = travelDistance;
 			// Adds answers to interests.
 			} else if (questionSet === 1) {
-				updateObject = {
-					'jobPreferences.answers.interest1': answers[0],
-					'jobPreferences.answers.interest2': answers[1],
-					'jobPreferences.answers.interest3': answers[2],
-					'jobPreferences.answers.interest4': answers[3],
-					'jobPreferences.answers.interest5': answers[4]
-				};
+				// updateObject = {
+				// 	'jobPreferences.answers.interest1': answers[0],
+				// 	'jobPreferences.answers.interest2': answers[1],
+				// 	'jobPreferences.answers.interest3': answers[2],
+				// 	'jobPreferences.answers.interest4': answers[3],
+				// 	'jobPreferences.answers.interest5': answers[4]
+				// };
+				const [interest1, interest2, interest3, interest4, interest5] = answers;
+				if (interest1?.trim()) updateObject['jobPreferences.answers.interest1'] = interest1;
+				if (interest2?.trim()) updateObject['jobPreferences.answers.interest2'] = interest2;
+				if (interest3?.trim()) updateObject['jobPreferences.asnwers.interest3'] = interest3;
+				if (interest4?.trim()) updateObject['jobPreferences.answers.interest4'] = interest4;
+				if (interest5?.trim()) updateObject['jobPreferences.answers.interest5'] = interest5;
 			}
 			// Updates preferences with new answers and the new date inputted.
-			await this.collection.updateOne(
-				{ discordId: userID },
-				{
-					$set: {
-						...updateObject,
-						'jobPreferences.userID': userID,
-						'jobPreferences.lastUpdated': new Date()
-					}
-				},
-				{ upsert: true }
-			);
+			if (Object.keys(updateObject).length > 0) {
+				await this.collection.updateOne(
+					{ discordId: userID },
+					{
+						$set: {
+							...updateObject,
+							'jobPreferences.userID': userID,
+							'jobPreferences.lastUpdated': new Date()
+						}
+					},
+					{ upsert: true }
+				);
+			}
 			return true;
 		} catch (error) {
 			console.error('Error storing job form responses', error);
