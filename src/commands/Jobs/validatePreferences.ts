b569fa4779838172e1452f17dc57208c2c37d553
rahul5400 <rahulp5400@yahoo.com>
2024-11-17 17:51:16 -0500

    finished making the error for employmentTypes

--- src/commands/Jobs/validatePreferences.ts
+++ src/commands/Jobs/validatePreferences.ts
@@ -20,5 +20,17 @@ export const validatePreferences = (answers: string[], qset: number, isJobForm: 
 				errors.push(`Invalid work type: ${invalidTypes.join(', ')}. Must be remote, hybrid, and/or in-person`);
 			}
 		}
+		if (!employmentType?.trim()) {
+			errors.push('Enter valid employment type');
+		} else {
+			const validEmploymentTypes = ['full time', 'part time', 'internship'];
+			// eslint-disable-next-line id-length
+			const employmentTypes = employmentType.toLowerCase().split(',').map(t => t.trim());
+			// eslint-disable-next-line id-length
+			const invalidTypes = employmentTypes.filter(t => !validEmploymentTypes.includes(t));
+			if (invalidTypes.length > 0) {
+				errors.push(`Invalid work type: ${invalidTypes.join(', ')}. Must be full time, part time, and/or internship`);
+			}
+		}
 	}
 };
