436f956284238c99c5c42aae4cc770d4ecca2acb
pgsweet <113042887+pgsweet@users.noreply.github.com>
2024-12-04 09:45:02 -0500

    Update commandManager.ts

--- src/pieces/commandManager.ts
+++ src/pieces/commandManager.ts
@@ -1,10 +1,9 @@
-import { Collection, Client, CommandInteraction, ApplicationCommand,
+import { Collection, Client, ApplicationCommand,
 	GuildMember, SelectMenuInteraction,
 	ModalSubmitInteraction, TextChannel, GuildMemberRoleManager,
 	ButtonInteraction, ModalBuilder, TextInputBuilder, ActionRowBuilder,
 	ModalActionRowComponentBuilder, ApplicationCommandType, ApplicationCommandDataResolvable, ChannelType, ApplicationCommandPermissionType, TextInputStyle,
-	ChatInputCommandInteraction,
-	ModalSubmitFields } from 'discord.js';
+	ChatInputCommandInteraction } from 'discord.js';
 import { isCmdEqual, readdirRecursive } from '@root/src/lib/utils/generalUtils';
 import { Command } from '@lib/types/Command';
 import { SageData } from '@lib/types/SageData';
