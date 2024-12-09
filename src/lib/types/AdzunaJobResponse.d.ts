bc757be4e141e26eb2e6d22f929d59993f044723
Hairum Qureshi <hqureshi@udel.edu>
2024-12-08 21:47:21 -0500

    created interface for the Adzuna Job API response

---
+++ src/lib/types/AdzunaJobResponse.d.ts
@@ -0,0 +1,16 @@
+/* eslint-disable camelcase */
+export interface AdzunaJobResponse {
+	company: {
+		display_name: string
+	};
+	title: string,
+	description: string,
+	location: {
+		display_name: string,
+		area: string
+	}
+	created: string;
+	salary_max: number | string;
+	salary_min: number | string;
+	redirect_url: string;
+}
