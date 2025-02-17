b736de637d9da1deb03aac5fa0a4eaffed9e21f7
John Aromando <jaro@udel.edu>
2025-02-17 13:20:18 -0500

    Create install-deps-windows.ps1

---
+++ install-deps-windows.ps1
@@ -0,0 +1,5 @@
+Write-Host "Installing system dependencies for Windows..."
+winget install -e --id GnuWin32.Make
+winget install -e --id Git.Git
+winget install -e --id StrawberryPerl.StrawberryPerl
+winget install -e --id Python.Python.3.10
