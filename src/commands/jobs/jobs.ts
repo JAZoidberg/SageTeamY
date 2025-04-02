37ecd4d3466556dcffbf2deb434cadfd9ef3c3e0
KKramer03 <kevinjkramer168@gmail.com>
2025-04-02 12:59:24 -0400

    MongoDb now works with job commands and changed command diff checker

--- src/commands/jobs/jobs.ts
+++ src/commands/jobs/jobs.ts
@@ -1,14 +1,14 @@
-import { ApplicationCommandOptionData, ApplicationCommandOptionType, AttachmentBuilder, ChatInputCommandInteraction, EmbedBuilder, InteractionResponse } from 'discord.js';
+import { ApplicationCommandOptionData, ApplicationCommandOptionType, AttachmentBuilder, ChatInputCommandInteraction, InteractionResponse } from 'discord.js';
 import fetchJobListings from '@root/src/lib/utils/jobUtils/Adzuna_job_search';
 import { JobResult } from '@root/src/lib/types/JobResult';
 import { Interest } from '@root/src/lib/types/Interest';
 import { JobData } from '@root/src/lib/types/JobData';
 import { Command } from '@lib/types/Command';
 import { DB, BOT, MAP_KEY } from '@root/config';
-import { Job } from '@root/src/lib/types/Job';
 import { MongoClient } from 'mongodb';
 import { sendToFile } from '@root/src/lib/utils/generalUtils';
 import axios from 'axios';
+import { JobPreferences } from '@root/src/lib/types/JobPreferences';
 
 
 export default class extends Command {
@@ -31,62 +31,36 @@ export default class extends Command {
 		}
 	]
 
-	// options: ApplicationCommandOptionData[] = [
-	// 	{
-	// 		name: 'question',
-	// 		description: 'The question you want to ask',
-	// 		type: ApplicationCommandOptionType.String,
-	// 		required: true
-	// 	}
-	// ]
-
-
 	async run(interaction: ChatInputCommandInteraction): Promise<void | InteractionResponse<boolean>> {
 		const userID = interaction.user.id;
 
 		const client = await MongoClient.connect(DB.CONNECTION, { useUnifiedTopology: true });
-		const db = client.db(BOT.NAME).collection(DB.JOB_FORMS);
-		const jobformAnswers:Job[] = await db.find({ owner: userID }).toArray();
-		// const jobData:JobData = {
-		// 	city: jobformAnswers[0].answers[0],
-		// 	preference: jobformAnswers[0].answers[1],
-		// 	jobType: jobformAnswers[0].answers[2],
-		// 	distance: jobformAnswers[0].answers[3],
-		// 	// filterBy: filterBy ?? 'default'
-		// 	filterBy: 'default'
-		// };
+		const db = client.db(BOT.NAME).collection(DB.USERS);
+		const filterBy = interaction.options.getString('filter') ?? 'default';
 
-		// const interests:Interest = {
-		// 	interest1: jobformAnswers[1].answers[0],
-		// 	interest2: jobformAnswers[1].answers[1],
-		// 	interest3: jobformAnswers[1].answers[2],
-		// 	interest4: jobformAnswers[1].answers[3],
-		// 	interest5: jobformAnswers[1].answers[4]
-		// };
+
+		const jobformAnswers: JobPreferences | null = (await db.findOne({ discordId: userID }))?.jobPreferences;
 
 		const jobData: JobData = {
-			city: 'New York',
-			preference: 'Biology',
-			jobType: 'Full Time',
-			distance: '10',
-			filterBy: 'date'
+			city: jobformAnswers.answers.city,
+			preference: jobformAnswers.answers.workType,
+			jobType: jobformAnswers.answers.employmentType,
+			distance: jobformAnswers.answers.travelDistance,
+			filterBy: 'default'
 		};
 
 		const interests: Interest = {
-			interest1: 'Construction',
-			interest2: 'Engineer',
-			interest3: 'Full Time',
-			interest4: 'New York',
-			interest5: '10'
+			interest1: jobformAnswers.answers.interest1,
+			interest2: jobformAnswers.answers.interest2,
+			interest3: jobformAnswers.answers.interest3,
+			interest4: jobformAnswers.answers.interest4,
+			interest5: jobformAnswers.answers.interest5
 		};
 
-
 		const APIResponse:JobResult[] = await fetchJobListings(jobData, interests);
-		const results = [jobData, interests, APIResponse];
 		const jobFormData: [JobData, Interest, JobResult[]] = [jobData, interests, APIResponse];
-		const filterBy = interaction.options.getString('filter') ?? 'default';
 
-		let message = `## Hey <@${userID}>!  
+		const message = `## Hey <@${userID}>!  
 			## Here's your list of job/internship recommendations:  
 Based on your interests in **${jobFormData[1].interest1}**, **${jobFormData[1].interest2}**, \
 **${jobFormData[1].interest3}**, **${jobFormData[1].interest4}**, and **${jobFormData[1].interest5}**, I've found these jobs you may find interesting. Please note that while you may get\
@@ -94,7 +68,7 @@ job/internship recommendations from the same company,\
 their positions/details/applications/salary WILL be different and this is not a glitch/bug!
 Here's your personalized list:
 
-			${this.listJobs(jobFormData[2], filterBy)}
+			${await this.listJobs(jobFormData, filterBy)}
 			
 			---  
 			### **Disclaimer:**  
@@ -106,26 +80,22 @@ Here's your personalized list:
 			`;
 
 		const attachments: AttachmentBuilder[] = [];
-		if (message.length > 2000) {
-			// message = `${message.substring(0, 1997)}...;
-
-			attachments.push(await sendToFile(this.stripMarkdown(message.split('---')[0], userID), 'txt', 'list-of-jobs-internships', false));
-			// interaction.user.send({ content: this.headerMessage(userID, 'default'), files: attachments as AttachmentBuilder[] });
-		}
-
 		const pubChan = interaction.channel;
-		if (pubChan) {
-			// pubChan.send({ content: message });
+		if (message.length > 2000) {
+			attachments.push(await sendToFile(this.stripMarkdown(message.split('---')[0], userID), 'txt', 'list-of-jobs-internships', false));
 			pubChan.send({ content: this.headerMessage(userID, filterBy), files: attachments as AttachmentBuilder[] });
+			// interaction.user.send({ content: this.headerMessage(userID, 'default'), files: attachments as AttachmentBuilder[] });
 		} else {
-			console.error('Channel not found');
+			pubChan.send(message);
 		}
 	}
 
-	listJobs(jobData: JobResult[], filterBy: string): string {
+	async listJobs(jobForm: [JobData, Interest, JobResult[]], filterBy: string): Promise<string> {
 		// Conditionally sort jobs by salary if sortBy is 'salary'
+		const cityCoordinates = await this.queryCoordinates(jobForm[0].city);
+
 		if (filterBy === 'salary') {
-			jobData.sort((a, b) => {
+			jobForm[2].sort((a, b) => {
 				const avgA = (Number(a.salaryMax) + Number(a.salaryMin)) / 2;
 				const avgB = (Number(b.salaryMax) + Number(b.salaryMin)) / 2;
 
@@ -136,39 +106,46 @@ Here's your personalized list:
 				return avgB - avgA; // Descending order
 			});
 		} else if (filterBy === 'alphabetical') {
-			jobData.sort((a, b) => (a.title > b.title ? 1 : -1));
-		}
-		else if (filterBy === 'date') {
-			jobData.sort((a, b) => new Date(b.created).getTime() - new Date(a.created).getTime());
-		}
-		else if (filterBy === 'distance') {
-			jobData.sort((a, b) => {
-				const distanceA = this.calculateDistance(Number(a.latitude), Number(a.longitude), Number(a.latitude), Number(a.longitude));
-				const distanceB = this.calculateDistance(Number(b.latitude), Number(b.longitude), Number(b.latitude), Number(b.longitude));
-				return distanceA - distanceB;
+			jobForm[2].sort((a, b) => (a.title > b.title ? 1 : -1));
+		} else if (filterBy === 'date') {
+			jobForm[2].sort((a, b) => new Date(b.created).getTime() - new Date(a.created).getTime());
+		} else if (filterBy === 'distance') {
+			// cityCoordinates = await this.queryCoordinates(jobForm[0].city);
+
+			jobForm[2].sort((a, b) => {
+				const distanceA = this.calculateDistance(cityCoordinates.lat, cityCoordinates.lng, Number(a.latitude), Number(a.longitude));
+				const distanceB = this.calculateDistance(cityCoordinates.lat, cityCoordinates.lng, Number(b.latitude), Number(b.longitude));
+
+				if (distanceA === -1) {
+					return 1; // Treat jobs with no location as lowest
+				}
+
+				return distanceA - distanceB; // Might have to account for negative distances
 			});
 		}
 
 
 
 		let jobList = '';
-		for (let i = 0; i < jobData.length; i++) {
-			const avgSalary = (Number(jobData[i].salaryMax) + Number(jobData[i].salaryMin)) / 2;
+		for (let i = 0; i < jobForm[2].length; i++) {
+			const avgSalary = (Number(jobForm[2][i].salaryMax) + Number(jobForm[2][i].salaryMin)) / 2;
 			const formattedAvgSalary = this.formatCurrency(avgSalary);
-			const formattedSalaryMax = this.formatCurrency(Number(jobData[i].salaryMax)) !== 'N/A' ? this.formatCurrency(Number(jobData[i].salaryMax)) : '';
-			const formattedSalaryMin = this.formatCurrency(Number(jobData[i].salaryMin)) !== 'N/A' ? this.formatCurrency(Number(jobData[i].salaryMin)) : '';
+			const formattedSalaryMax = this.formatCurrency(Number(jobForm[2][i].salaryMax)) !== 'N/A' ? this.formatCurrency(Number(jobForm[2][i].salaryMax)) : '';
+			const formattedSalaryMin = this.formatCurrency(Number(jobForm[2][i].salaryMin)) !== 'N/A' ? this.formatCurrency(Number(jobForm[2][i].salaryMin)) : '';
+			const jobDistance = this.calculateDistance(cityCoordinates.lat, cityCoordinates.lng, Number(jobForm[2][i].latitude), Number(jobForm[2][i].longitude));
+			const formattedDistance = (jobDistance !== -1) ? `${jobDistance.toFixed(2)} miles` : 'N/A';
 
 			const salaryDetails = (formattedSalaryMin && formattedSalaryMax)
 				? `, Min: ${formattedSalaryMin}, Max: ${formattedSalaryMax}`
 				: formattedAvgSalary;
 
-			jobList += `${i + 1}. **${jobData[i].title}**  
+			jobList += `${i + 1}. **${jobForm[2][i].title}**  
 			  \t\t* **Salary Average:** ${formattedAvgSalary}${salaryDetails}  
-			  \t\t* **Location:** ${jobData[i].location}  
-			  \t\t* **Date Posted:** ${new Date(jobData[i].created).toDateString()} at ${new Date(jobData[i].created).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}
-			  \t\t* **Apply here:** [read more about the job and apply here](${jobData[i].link})  
-			  \t\t* **Distance:** ${this.calculateDistance(Number(jobData[i].latitude), Number(jobData[i].longitude), Number(jobData[i].latitude), Number(jobData[i].longitude))} miles
-			  ${i !== jobData.length - 1 ? '\n' : ''}`;
+			  \t\t* **Location:** ${jobForm[2][i].location}  
+			  \t\t* **Date Posted:** ${new Date(jobForm[2][i].created).toDateString()} at ${new Date(jobForm[2][i].created).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}
+			  \t\t* **Apply here:** [read more about the job and apply here](${jobForm[2][i].link})  
+			  \t\t* **Distance:** ${formattedDistance}
+			  ${i !== jobForm[2].length - 1 ? '\n' : ''}`;
 		}
 
 		return jobList || '### Unfortunately, there were no jobs found based on your interests :(. Consider updating your interests or waiting until something is found.';
@@ -216,15 +193,14 @@ Here's your personalized list:
 			Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
 			Math.sin(dLon / 2) * Math.sin(dLon / 2);
 		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-		const distance = R * c;
-
+		const distance = (lat1 === 0 && lon1 === 0) || (lat2 === 0 && lon2 === 0) ? -1 : R * c;
 		return distance;
 	}
 
 	async queryCoordinates(location: string): Promise<any> { // Change to appropriate type later
 		const preferredCity = encodeURIComponent(location);
 
-		const baseURL = 'https://maps.google.com/maps/api/geocode/json?address=${preferredCity}&components=country:US&key=${MAP_KEY}';
+		const baseURL = `https://maps.google.com/maps/api/geocode/json?address=${preferredCity}&components=country:US&key=${MAP_KEY}`;
 		const response = await axios.get(baseURL);
 		const coordinates : {lat: number, lng: number } = {
 			lat: response.data.results[0].geometry.location.lat,
---
+++ src/lib/types/JobPreferences.d.ts
@@ -0,0 +1,13 @@
+export interface JobPreferences {
+	answers: {
+		city: string;
+		workType: string;
+		employmentType: string;
+		travelDistance: string;
+		interest1: string;
+		interest2: string;
+		interest3: string;
+		interest4: string;
+		interest5: string;
+	};
+}
